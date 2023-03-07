8dbef816ddc05d5d0a3649356489007eb684abcd
dgjinsu <rlawlstn3206@naver.com>
2023-03-07 21:14:07 +0900

    feat: 리액트 연동용 테스트 코드 추가
    리액트 연동용 테스트 코드 추가

--- backend/src/main/java/linklibrary/controller/TestController.java
+++ backend/src/main/java/linklibrary/controller/TestController.java
@@ -9,4 +9,9 @@ public class TestController {
     public String test() {
         return "test 데이터 입니다";
     }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "test 데이터 입니다22";
+    }
 }

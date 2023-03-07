9d221a98becc6b5dda351a581f5c4ef66555db93
dgjinsu <rlawlstn3206@naver.com>
2023-03-07 19:14:34 +0900

    feat: 백엔드 개발 환경 구축
    JPA, Lombok, security, swagger, p6spy, querydsl 에 대한 dependencies 설정
    application.yml 작성

---
+++ backend/src/main/java/linklibrary/LinkLibraryApplication.java
@@ -0,0 +1,13 @@
+package linklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkLibraryApplication.class, args);
+	}
+
+}

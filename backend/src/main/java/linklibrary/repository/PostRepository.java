3d1e119fa59dfb2ee860592dba9a8b361c289fcb
dgjinsu <rlawlstn3206@naver.com>
2023-03-12 17:40:03 +0900

    feat: entity 개발
    entity 개발

---
+++ backend/src/main/java/linklibrary/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package linklibrary.repository;
+
+import linklibrary.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}

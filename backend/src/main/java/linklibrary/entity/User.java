9eb0442922a1d23d703028d664c0ecd1585db434
dgjinsu <rlawlstn3206@naver.com>
2023-03-12 16:19:51 +0900

    refactor: User 엔티티 어노테이션 추가

--- backend/src/main/java/linklibrary/entity/User.java
+++ backend/src/main/java/linklibrary/entity/User.java
@@ -1,5 +1,7 @@
 package linklibrary.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -7,6 +9,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name ="Users")
+@Data
 public class User {
 
     @Id @GeneratedValue

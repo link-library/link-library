0d4e0d874f2520e0fcc841f5c19c8fbb98e392d1
plan11plan <pmsuk7028@naver.com>
2023-03-12 16:11:41 +0900

    refactor: User 클래스 명 변경
    Users -> User로 클래스 명 변경

--- backend/src/main/java/linklibrary/entity/User.java
+++ backend/src/main/java/linklibrary/entity/User.java
@@ -3,9 +3,11 @@ package linklibrary.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-public class Users {
+@Table(name ="Users")
+public class User {
 
     @Id @GeneratedValue
     private Long id;

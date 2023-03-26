0f684329683e8be03a413c56efad60df6444d099
dgjinsu <rlawlstn3206@naver.com>
2023-03-26 17:53:28 +0900

    refactor: 토큰 만료 시간 60분으로 수정
    refactor: 토큰 만료 시간 60분으로 수정

--- backend/src/main/java/linklibrary/security/jwt/JwtProperties.java
+++ backend/src/main/java/linklibrary/security/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package linklibrary.security.jwt;
 
 public interface JwtProperties {
     String SECRET = "lzighesooiehfoiehdddosizgaeqwoipvdhvkxljkhaodqddafagegrdhpiogzuskeffhzos";
-    int EXPIRATION_TIME = 100000 * 60 * 10 * 6; //60분
+    int EXPIRATION_TIME = 100000 * 60 * 10 * 6;//60분
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }

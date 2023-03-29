e255a8aec3ad4b3b7640c2af969649be9cf37dac
dgjinsu <rlawlstn3206@naver.com>
2023-03-29 20:38:13 +0900

    feat: postService 테스트 코드 작성
    조회를 제외한 생성, 삽입, 수정 테스트 코드 작성 완료

--- backend/src/test/java/linklibrary/service/PostServiceTest.java
+++ backend/src/test/java/linklibrary/service/PostServiceTest.java
@@ -8,20 +8,22 @@ import linklibrary.entity.User;
 import linklibrary.repository.PostRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 class PostServiceTest {
-
     @Autowired
     EntityManager em;
-
     @Autowired
     PostService postService;
     @Autowired
@@ -36,11 +38,51 @@ class PostServiceTest {
         //when
         Long savedPostId = postService.createPost(postFormDto, user);
         //then
+        em.flush();
+        em.clear();
         Post post = postRepository.findById(savedPostId).get();
         assertThat(post.getTitle()).isEqualTo(postFormDto.getTitle()); //title 비교
         assertThat(post.getMemo()).isEqualTo(postFormDto.getMemo()); //memo 비교
         assertThat(post.getUrl()).isEqualTo(postFormDto.getUrl()); //url 비교
         assertThat(post.getCategory().getId()).isEqualTo(postFormDto.getCategoryId()); //카테고리 id 비교
+    }
+
+    @Test
+    public void 포스트수정() throws Exception {
+        //given
+        User user = createUser();
+        Category category = createCategory(user);
+        Post post = createPost(user, category);
+        PostFormDto postForm = createPostForm(category);
+        postForm.setTitle("바뀐제목1");
+        //when
+        Long updatedPostId = postService.change(post.getId(), postForm);
+        //then
+        em.flush();
+        em.clear();
+        Post updatedPost = postRepository.findById(updatedPostId).get();
+        assertThat(updatedPost.getTitle()).isEqualTo(postForm.getTitle());
+    }
+
+    @Test
+    public void 포스트삭제() throws Exception {
+        //given
+        User user = createUser();
+        Category category = createCategory(user);
+        Post post = createPost(user, category);
+        //when
+        postService.deletePost(post.getId());
+        //then
+        em.flush();
+        em.clear();
+        Optional<Post> findPost = postRepository.findById(post.getId());
+        assertThat(findPost).isEmpty();
+    }
+
+    private Post createPost(User user, Category category) {
+        Post post = Post.builder().title("제목1").memo("메모1").url("www.abc.com").bookmark(true).category(category).user(user).build();
+        em.persist(post);
+        return post;
     }
 
     private PostFormDto createPostForm(Category category) {

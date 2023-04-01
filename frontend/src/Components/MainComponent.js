046077ab6846cb088f928fb715171232bcb7bdc5
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-04-01 18:56:46 +0900

    refactor: 메인페이지 1차 리팩토링

--- frontend/src/Components/MainComponent.js
+++ frontend/src/Components/MainComponent.js
@@ -1,33 +1,11 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
-import { isSidebarOpenState } from '../atoms';
-import Sidebar from './Sidebar';
 
 export const MainComponent = () => {
-  const isSidebarOpen = useRecoilValue(isSidebarOpenState);
   return (
-    <div
-      style={{
-        // 메인 화면 컴포넌트가 오른쪽으로 밀리는 효과
-        display: 'flex',
-        flexDirection: 'row',
-        backgroundColor: '#e7f5ff',
-        marginLeft: isSidebarOpen ? '250px' : '0',
-        transition: 'margin-left 0.5s ease-in-out',
-        position: 'absolute',
-        top: '70px',
-        left: '0',
-        bottom: '0',
-        right: '0',
-      }}
-    >
-      <Sidebar />
-      <div style={{ flexGrow: 1, marginLeft: '20px' }}>
-        <h1>헤더 테스트</h1>
-        <p>링크 카드 배치</p>
-      </div>
+    <div style={{ flexGrow: 1, marginLeft: '20px' }}>
+      <h1>헤더 테스트</h1>
+      <p>링크 카드 배치</p>
     </div>
   );
 };
-
 export default MainComponent;

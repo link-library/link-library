ee6ed547f8f081902d3e1749ed3ea6a5623a58cd
plan11plan <pmsuk7028@naver.com>
2023-05-18 01:07:58 +0900

    Category 엔티티 orphanRemoval = true 추가
    Category 엔티티 orphanRemoval = true 추가

--- backend/src/main/java/linklibrary/entity/Category.java
+++ backend/src/main/java/linklibrary/entity/Category.java
@@ -32,7 +32,7 @@ public class Category {
     private User user;
 
     @Builder.Default
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL) //  2023.05.18 cascade 추가 
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true) //  2023.05.18 cascade 추가
     private List<Post> posts = new ArrayList<>();
 
 

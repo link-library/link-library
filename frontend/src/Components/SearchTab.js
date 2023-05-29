9f3fd92daa85fd4b07524b5d2477eb4cba722db9
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-05-29 18:17:30 +0900

    feat: 포스트 검색 기능 추가 완료

--- frontend/src/Components/SearchTab.js
+++ frontend/src/Components/SearchTab.js
@@ -2,16 +2,48 @@ import React, { useState } from 'react';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 import { Box, IconButton } from '@mui/material';
+import { getPostDataByKeyword } from '../Pages/Async';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import {
+  postDataState,
+  postPageState,
+  searchPostByKeywordState,
+  selectedCategoryIdState,
+  selectedCategoryNameState,
+  totalPostAmountBySelectedCategoryState,
+} from '../atoms';
 
 const SearchTab = () => {
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useRecoilState(
+    searchPostByKeywordState
+  );
+  const [postcards, setPostcards] = useRecoilState(postDataState);
+  const setTotalPostAmount = useSetRecoilState(
+    totalPostAmountBySelectedCategoryState
+  );
+  const [page, setPage] = useRecoilState(postPageState);
+  const setSelectedCategoryId = useSetRecoilState(selectedCategoryIdState);
+  const setSelectedCategoryName = useSetRecoilState(selectedCategoryNameState);
 
   const handleInputChange = (event) => {
     setSearchValue(event.target.value);
   };
 
-  const handleSearchClick = () => {
+  const handleSearchClick = async () => {
     // 검색 버튼 핸들러
+    console.log('clicked the search btn');
+    const { message, postData, totalPostAmount } = await getPostDataByKeyword(
+      0,
+      searchValue
+    );
+    if (message === '찜목록 or 전체페이지 조회 완료') {
+      console.log(`${searchValue}로 검색된 값 목록 조회 완료`);
+      setPostcards(postData);
+      setTotalPostAmount(totalPostAmount);
+      setPage(1);
+      setSelectedCategoryId(-2);
+      setSelectedCategoryName('검색된 링크 카드 목록');
+    }
   };
 
   return (

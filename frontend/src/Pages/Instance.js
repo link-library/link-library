a2b2f83760c39bae8dd658dcde581a1866a84983
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-06-29 20:11:36 +0900

    feat: workflows 추가

--- frontend/src/Pages/Instance.js
+++ frontend/src/Pages/Instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://3.36.221.184:3000',
   headers: {
     'Content-Type': 'application/json',
   },
